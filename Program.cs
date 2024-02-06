406a20f177622da3c7a213d49cccd7ff23408258
Jennie Galmin <Jennie.galmin@gmail.com>
2024-02-06 15:33:17 +0100

    Får upp rätt namn

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-       ServerClient.Client();
+        ServerClient.Client();
     }
 }
--- ServerClient.cs
+++ ServerClient.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 
+
 public class ServerClient{
     public static void Client(){
         
@@ -15,14 +16,26 @@ public class ServerClient{
         );
 
         socket.Connect(iPEndPoint);
-        string request = "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
-        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(request);
 
-        socket.Send(buffer);
-        byte[] incoming = new byte[10000];
-        int read = socket.Receive(incoming);
-        string response = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
-        Console.WriteLine($"Response: {response}");
-        socket.Close();
+        Console.WriteLine("Skriv in ditt användarnamn");
+        string? username = Console.ReadLine();
+        Console.WriteLine("Skriv in ditt lösenord");
+        string? password = Console.ReadLine();
+
+        string credentials = $"{username}:{password}";
+        // sparar username och password i en variabel
+
+        byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(credentials);
+        // gör om svaret till bytes för att skicka till server
+        socket.Send(sendBuffer);
+        // skickar till servern
+        byte[] receiveBuffer = new byte[5000];
+       int read = socket.Receive(receiveBuffer);
+        // tar emot svar från servern
+        string response = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, read);
+        // gör om svaret till en string
+       Console.WriteLine($"{response}");
+        
+
     }
 }
